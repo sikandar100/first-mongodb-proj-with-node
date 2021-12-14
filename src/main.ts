30ca16cc0a3f6aec9cdf924047f8c776320f8747
Sikandar Waheed <ch.sikandar100@gmail.com>
2021-12-14 10:09:44 +0500

    Feat: Initial Setup with NestJs

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log('server is running at 3000 port');
+}
+bootstrap();
